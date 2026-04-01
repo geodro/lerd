101f657c1634fba5ed9f49b20371a47c82ea12c5
George Dumitrescu <george@dumitres.co>
2026-04-01 14:06:00 +0300

    chore: bump version to 1.3.1-alpha.10

--- internal/version/version.go
+++ internal/version/version.go
@@ -9,7 +9,7 @@ import "fmt"
 //	-X github.com/geodro/lerd/internal/version.Commit=<sha>
 //	-X github.com/geodro/lerd/internal/version.Date=<iso8601>
 var (
-	Version = "1.3.1-alpha.9"
+	Version = "1.3.1-alpha.10"
 	Commit  = "none"
 	Date    = "unknown"
 )
